46f80c6ea1d480d804440c8c5238360deb5d56c4
Nicki Hvolgaard <38239769+TheMagicalKing@users.noreply.github.com>
2020-04-18 13:51:03 +0200

    oprettet NewGol og følger Youtube tutorial https://www.youtube.com/watch?v=ncRA1OAbMmk

---
+++ src/Game.java
@@ -0,0 +1,26 @@
+import java.lang.reflect.Array;
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.stage.Stage;
+
+public class Game {
+
+    private int[][] gameField = {
+            { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+            { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+            { 0, 0, 0, 1, 0, 1, 0, 0, 0, 0 },
+            { 0, 0, 1, 0, 1, 0, 1, 0, 0, 0 },
+            { 0, 0, 0, 1, 0, 1, 0, 0, 0, 0 },
+            { 0, 0, 1, 0, 1, 0, 1, 0, 0, 0 },
+            { 0, 0, 0, 1, 0, 1, 0, 0, 0, 0 },
+            { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+            { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+            { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }
+    };
+
+}
